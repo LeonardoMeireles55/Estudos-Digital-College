7899b31c62faa4dc95bed36625f31d82114d0ebf
LeonardoMeireles55 <leomeireles55@hotmail.com>
2023-02-05 18:27:45 -0300

    update

---
+++ cod3r/funcao/arrowFunction2.js
@@ -0,0 +1,17 @@
+let comparaComThis = function(param) {
+    console.log(this === param)
+}
+console.log(comparaComThis())
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
