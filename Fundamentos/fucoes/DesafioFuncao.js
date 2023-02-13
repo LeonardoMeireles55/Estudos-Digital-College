e82ab718c67eb11997e9172afaccd9e784c94dcd
LeonardoMeireles55 <123477726+LeonardoMeireles55@users.noreply.github.com>
2023-02-13 17:53:35 -0300

    Delete Fundamentos/fucoes directory

--- Fundamentos/fucoes/DesafioFuncao.js
+++
@@ -1,8 +0,0 @@
-// 2 params -> console.log(+)
-// 2 params -> console.log(-)
-
-function soma(a, b) {
-console.log(a + b)
-}
-soma(3, 2)
-
--- Fundamentos/fucoes/undefined.js
+++
@@ -1,4 +0,0 @@
-let a; // não inicializado o valor da variavel. 
-let c = null;
-let d = undefined;
-console.log(a,d,c);
