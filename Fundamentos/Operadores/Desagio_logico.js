9481066406c7180d92b1862f822166e35fe7130f
LeonardoMeireles55 <leomeireles55@hotmail.com>
2023-02-26 18:25:44 -0300

    update

--- Fundamentos/Operadores/Desagio_logico.js
+++ Fundamentos/Operadores/Desagio_logico.js
@@ -1,8 +1,14 @@
-// // Duas expressões resultar em true(aritimetico, relacionais, logicos)
-// let verdadeiro1 = 
-// let verdadeiro2 = 
+// Duas expressões resultar em true(aritimetico, relacionais, logicos)
+let verdadeiro1 = 2 + 5 >= 7 && 8 > 2;
+let verdadeiro2 = !(6 * 6 < 3); // o operador !(negação) inverte para true. 
 
-// console.log(verdadeiro1);
-// console.log(verdeiro2);
+console.log(verdadeiro1);
+console.log(verdadeiro2);
+//Duas expressões resultar em false(aritimetico, relacionais, logicos)
+let falso1 = 8 - 4 > 12 || false || 15 === "15"; 
+let falso2 = 7 !== 7 || "3" !== 3;
+let test = "3" == 3 
 
-// //let falso 1
+console.log(falso1);
+console.log(falso2);
+console.log(test);
