faf5746342e453912f52105450d004cc964aa2e7
LeonardoMeireles55 <leomeireles55@hotmail.com>
2023-03-11 21:32:10 -0300

    update

--- Unifor_ADS/megasena.py
+++ Unifor_ADS/megasena.py
@@ -1,7 +1,7 @@
 # importa o módulo random do Python. Esse módulo contém funções que geram números aleatórios.
 import random
 # define uma variável N com o valor 6. Essa variável indica quantos números devem ser sorteados para a aposta.
-n = 6
+n = 7
 # função random.sample() para gerar 6 números aleatórios únicos.
 sorteados = random.sample(range(1, 61), n)
 # Mostrar na tela resultado com mensagem.
